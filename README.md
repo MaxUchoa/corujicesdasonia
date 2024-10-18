e24216588194b24a805ba10f9072207f6abf5e5f
MaxUchoa <60898174+MaxUchoa@users.noreply.github.com>
2024-10-17 21:16:36 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# corujicesdasonia
+# artesanato
+ 
---
+++ javascript.js
@@ -0,0 +1,28 @@
+let myCarousel = document.querySelectorAll('#carrosselContainer .carousel .carousel-item');
+
+myCarousel.forEach((el) => {
+  const minPerSlide = 4;
+  let next = el.nextElementSibling;
+
+  for (let i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // Volta ao primeiro item se não houver mais próximos
+      next = myCarousel[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
+
+$(document).ready(function() {
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').stop().animate({
+        scrollTop: target.offset().top - 100 // Ajuste para que o título não fique coberto
+      }, 1000); // Tempo em milissegundos para a animação
+    }
+  });
+});
